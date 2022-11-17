6f2814da92023385da3b5885f842e5d95fd07c2d
Maxim Karachyov <karachyov.m@gmail.com>
2022-11-17 16:23:08 +0300

    js

--- src/js/blackfriday22.js
+++ src/js/blackfriday22.js
@@ -1,5 +1,10 @@
-@@include('./partials/jquery.formstyler.js')
-
-setTimeout(function() {
-    $('.js-select').styler();
-}, 100)
+$(function (){
+    $('.blackfriday22-block__btn').on('mouseenter', function (e){
+        let idCatalog = $(this).data('id');
+        $(this).parents('.blackfriday22-block__body').find('.blackfriday22-category__item').removeClass('active');
+        $(this).parents('.blackfriday22-block__body').find('[data-catalog='+idCatalog+']').addClass('active');
+    })
+    $('.blackfriday22-block__btn').on('mouseleave', function (e){
+        $(this).parents('.blackfriday22-block__body').find('.blackfriday22-category__item').removeClass('active');
+    })
+})
